d17753c3c35cce348f85e2e5b3b8c863c17efc74
WalkerSumida <walkersumida@gmail.com>
2019-04-22 23:17:33 +0900

    feat: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Demo user
+email = 'demo@xxx.com'
+if User.find_by(email: email).blank?
+  User.create(uid: '1234', email: email, password: 'demodemo')
+end
--- docker/bin/rails_start.sh
+++ docker/bin/rails_start.sh
@@ -1,5 +1,6 @@
 bundle exec rake db:create
 bundle exec rake db:migrate
+bundle exec rake db:seed
 [ -f /tmp/rails_api.pid ] && rm /tmp/rails_api.pid || echo "Not found"
 bundle exec rails s -P /tmp/rails_api.pid -b '0.0.0.0'
 
