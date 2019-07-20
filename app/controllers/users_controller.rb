1e3667e5178d468e4ff492db184c3c14ae249a37
WalkerSumida <walkersumida@gmail.com>
2019-07-20 17:54:31 +0900

    feat: add users show and update endpoints

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < AuthenticatedController
+  def show
+    # noop
+  end
+
+  def update
+    if current_user.update(user_params)
+      render :show
+    else
+      render json: current_user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname)
+  end
+end
---
+++ app/views/users/_user.json.jbuilder
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+json.id user.uuid
+json.nickname user.nickname
