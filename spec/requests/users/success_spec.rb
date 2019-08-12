30e14e3d687a8577f3d6a7bb563f671061c203fa
WalkerSumida <walkersumida@gmail.com>
2019-08-12 18:28:53 +0900

    test: change response users.id

---
+++ spec/requests/users/success_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'POST /auth/sign_in', type: :request do
+  let(:email) { user.email }
+  let(:password) { 'password' }
+  let(:user) { create(:user, password: password, nickname: 'nickname') }
+  let(:valid_attributes) do
+    {
+      email: email,
+      password: password,
+    }
+  end
+  let(:params) do
+    valid_attributes
+  end
+
+  it 'get user' do
+    is_expected.to eq(200)
+
+    expect(response.body).to be_json_as(
+      {
+        id: user.uuid,
+        email: user.email,
+        name: user.name,
+        nickname: user.nickname,
+        image: user.image,
+      }
+    )
+  end
+end
+
