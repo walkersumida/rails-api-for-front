46733197a7c202e7ea7e739a489f2ab29de0e641
WalkerSumida <walkersumida@gmail.com>
2019-08-14 22:42:35 +0900

    refact: add frozen_string_literal

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class SessionsController < DeviseTokenAuth::SessionsController
   def render_create_success
     render 'users/success', formats: :json
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class UsersController < AuthenticatedController
   def show
     # noop
