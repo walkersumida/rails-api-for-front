6fbe556b4ad8f0edaceecc86ce73683d1ac02404
WalkerSumida <walkersumida@gmail.com>
2019-08-24 21:12:19 +0900

    test: add spec(uuid param is `me`)

--- spec/requests/users/show_spec.rb
+++ spec/requests/users/show_spec.rb
@@ -1,22 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe 'GET /users/:uuid', type: :request do
-  let(:uuid) { user.uuid }
   let(:user) { create(:user) }
 
   sign_in(:user)
 
-  it 'get user' do
-    is_expected.to eq(200)
+  context ':uuid param is uuid' do
+    let(:uuid) { user.uuid }
 
-    expect(response.body).to be_json_as(
-      {
-        id: user.uuid,
-        email: user.email,
-        name: user.name,
-        nickname: user.nickname,
-        image: user.image,
-      }
-    )
+    it 'get user' do
+      is_expected.to eq(200)
+
+      expect(response.body).to be_json_as(
+        {
+          id: user.uuid,
+          email: user.email,
+          name: user.name,
+          nickname: user.nickname,
+          image: user.image,
+        }
+      )
+    end
+  end
+
+  context ':uuid param is `me`' do
+    let(:uuid) { 'me' }
+
+    it 'get user' do
+      is_expected.to eq(200)
+
+      expect(response.body).to be_json_as(
+        {
+          id: user.uuid,
+          email: user.email,
+          name: user.name,
+          nickname: user.nickname,
+          image: user.image,
+        }
+      )
+    end
   end
 end
