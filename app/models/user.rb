ba3397087c708e2bb982de4b332ba0615a1b54e3
WalkerSumida <walkersumida@gmail.com>
2019-10-13 17:38:42 +0900

    feat: add carrierwave logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
+require 'carrierwave/orm/activerecord'
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  mount_uploader :image, Uploaders::UserProfile
 
   has_many :posts, dependent: :destroy
 
